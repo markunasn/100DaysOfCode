e1fa6a284161d87b0066826be60bae646b2fa8e6
Nick <markunasn@gmail.com>
2022-03-01 23:15:37 -0600

    Day 9 - Dictionaries (Project: Silent Auction)

--- Beginner_Section_Days_1-14/Day9.py
+++ Beginner_Section_Days_1-14/Day9.py
@@ -7,4 +7,47 @@ Project:
 -Silent Auction
 '''
 
-#test
+'''
+#test grades
+student_scores = {"Jim": 62, "Joe": 74, "Kate": 96, "Aly": 83, "Alex": 55}
+grades = {}
+
+for student in student_scores:
+    score = student_scores[student]
+    if score > 90:
+        grades[student] = 'A'
+    elif score > 80:
+        grades[student] = 'B'
+    elif score > 70:
+        grades[student] = 'C'
+    elif score > 60:
+        grades[student] = 'D'
+    else:
+        grades[student] = 'F'
+
+print(grades)
+'''
+
+#Secret Auction
+clear = "\n" * 100
+other_bidders = True
+bids = {"opening" : 0}
+while(other_bidders):
+    name = input("What is your name? ")
+    bid = int(input("What is your bid?"))
+    bids[name] = bid
+    loop = input("Are there any other bidders? (y/n): ")
+    if loop == 'n':
+        other_bidders = False
+    else:
+        print(clear)
+
+winner = 'opening'
+#assuming no bids are the sake for simplicity
+#todo add tie logic
+for name in bids:
+    if bids[name] > bids[winner]:
+        winner = name
+
+
+print("The winner of the auction is " + winner + " with a bid of $" + str(bids[winner]))
